07e202fbba8f58163b086db1c07105015f40e3bc
juancarlucci <juancarloscollins@gmail.com>
2024-05-06 14:35:35 -0700

    updates server database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 
 const mockURLs = [
-  "https://utfs.io/f/bb7ad2d8-ad7d-4f25-9fa5-d22f21c7270d-ja4b6k.jpg",
+  "https://utfs.io/f/c0b85663-2e9f-4f3a-bbaa-bf89081d5344-ja4b6j.jpg",
   "https://utfs.io/f/33c65d9d-39ba-4212-9496-4f4640138ffa-ja4b6k.jpg",
+  "https://utfs.io/f/8455df63-777c-4cfc-bc31-c46ed7ec0195-hhcy55.jpg",
   "https://utfs.io/f/d92b6e39-e7eb-4b85-96a2-b45ed007ce9c-jelt2e.jpg",
   "https://utfs.io/f/2a0f00e7-c1e5-4199-a836-5e01032f1fc0-7b1ezs.jpg",
   "https://utfs.io/f/f81271e1-102d-45e2-b680-807049297791-2zs7o5.jpg",
-  "https://utfs.io/f/8455df63-777c-4cfc-bc31-c46ed7ec0195-hhcy55.jpg",
- 
 ];
 
 const mockImages = mockURLs.map((url, index) => ({
