19dbf7130d8209637674002ead4d22c4cd333128
juancarlucci <juancarloscollins@gmail.com>
2024-05-09 11:23:07 -0700

    fix: typeof schema

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,5 +2,4 @@ import { drizzle } from "drizzle-orm/vercel-postgres-js";
 import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-export const db = drizzle(sql, {schema});
+export const db = drizzle<sql.Sql, typeof schema>(sql, {schema});
